7fc369f4d4faf3beb9bf8d5982f977c5776ac776
Ayman Said <ayman-said@live.com>
2019-07-27 20:54:57 +0300

    Password Strength Meter

--- assets/js/global.js
+++ assets/js/global.js
@@ -52,22 +52,23 @@ $( document ).ready(function() {
   });
 
   // Default behavior
-  // $('#id_password1').password();
-  $('#password').password({
-  shortPass: 'The password is too short',
-  badPass: 'Weak; try combining letters & numbers',
-  goodPass: 'Medium; try using special characters',
-  strongPass: 'Strong password',
-  containsField: 'The password contains your username',
-  enterPass: 'Type your password',
-  showPercent: false,
-  showText: true, // shows the text tips
-  animate: true, // whether or not to animate the progress bar on input blur/focus
-  animateSpeed: 'fast', // the above animation speed
-  field: false, // select the match field (selector or jQuery instance) for better password checks
-  fieldPartialMatch: true, // whether to check for partials in field
-  minimumLength: 4 // minimum password length (below this threshold, the score is 0)
-});
+  $('#id_password1').password();
+  $('#id_new_password1').password();
+//   $('#id_password1').password({
+//   shortPass: 'The password is too short',
+//   badPass: 'Weak; try combining letters & numbers',
+//   goodPass: 'Medium; try using special characters',
+//   strongPass: 'Strong password',
+//   containsField: 'The password contains your username',
+//   enterPass: 'Type your password',
+//   showPercent: false,
+//   showText: true, // shows the text tips
+//   animate: true, // whether or not to animate the progress bar on input blur/focus
+//   animateSpeed: 'fast', // the above animation speed
+//   field: false, // select the match field (selector or jQuery instance) for better password checks
+//   fieldPartialMatch: true, // whether to check for partials in field
+//   minimumLength: 4 // minimum password length (below this threshold, the score is 0)
+// });
 
 
 });
